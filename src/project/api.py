10605b79d40d2ba579b79416a9f862dace0b7c00
luka <luka.asfari@gmail.com>
2024-11-09 16:38:29 +0100

    Initial commit

---
+++ src/project/api.py
@@ -0,0 +1,23 @@
+from ninja import NinjaAPI, Schema
+
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.controller import NinjaJWTDefaultController
+
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
+
+class UserSchema(Schema):
+    username: str = ""
+    is_authenticated: bool = False
+    email: str = None 
+
+@api.get('/hello')
+def hello(request):
+    print(request)
+    return 'Hello World!'
+
+@api.get('/me', response=UserSchema, auth=JWTAuth())
+def me(request):
+    print(request.user)
+    return request.user 
