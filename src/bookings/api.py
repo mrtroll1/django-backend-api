ea0cd1fb708132522b83b348b88df6e1fe9ec915
luka <luka.asfari@gmail.com>
2024-11-09 23:12:26 +0100

    add auth protection to bookings endpoints

--- src/bookings/api.py
+++ src/bookings/api.py
@@ -2,6 +2,8 @@ from typing import List
 
 from ninja import Router
 
+from ninja_jwt.authentication import JWTAuth
+
 from django.shortcuts import get_object_or_404
 
 from .models import Booking
@@ -9,11 +11,11 @@ from .schemas import BookingListSchema, BookingDetailSchema
 
 router = Router()
 
-@router.get('', response=List[BookingListSchema])
+@router.get('', response=List[BookingListSchema], auth=JWTAuth())
 def bookings_list(request):
     return Booking.objects.all()
 
-@router.get('{entry_id}/', response=List[BookingDetailSchema])
+@router.get('{entry_id}/', response=List[BookingDetailSchema], auth=JWTAuth())
 def booking_detail(request, entry_id:int):
     qs = [get_object_or_404(Booking, id=entry_id)]
     return qs
